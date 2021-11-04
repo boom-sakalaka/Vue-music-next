bffad9106742021386f66e4f4138d61469c7f095
Gu <936285702@qq.com>
2021-11-04 21:19:33 +0800

    添加图片懒加载  ---vue3-lazy

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-11-02 23:19:22
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-11-04 21:17:57
+ * @FilePath: \vue-music-next\src\main.js
+ * @Description:
+ */
+/* eslint-disable global-require */
 import { createApp } from 'vue';
+import lazyPlugin from 'vue3-lazy';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -6,4 +16,10 @@ import store from './store';
 // 引入全局样式文件
 import '@/assets/scss/index.scss';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(lazyPlugin, {
+    loading: require('@/assets/images/default.png'),
+  })
+  .mount('#app');
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -2,7 +2,7 @@
  * @Author: GZH
  * @Date: 2021-11-03 20:57:52
  * @LastEditors: GZH
- * @LastEditTime: 2021-11-04 20:47:16
+ * @LastEditTime: 2021-11-04 21:18:51
  * @FilePath: \vue-music-next\src\views\recommend.vue
  * @Description:
 -->
@@ -20,7 +20,7 @@
           <ul>
             <li v-for="item in albums" class="item" :key="item.id">
               <div class="icon">
-                <img width="60" height="60" :src="item.pic" />
+                <img width="60" height="60" v-lazy="item.pic" :src="item.pic" />
               </div>
               <div class="text">
                 <h2 class="name">
